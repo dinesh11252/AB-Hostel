06cf47ee15fe21c6fda176e8f4a6d67400ce34bc
dinesh11252 <126875764+dinesh11252@users.noreply.github.com>
2023-03-03 17:11:02 +0530

    Add files via upload

---
+++ backend model project/controllers/employeeController.js
@@ -0,0 +1,124 @@
+const express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+const Employee = mongoose.model('Employee');
+
+router.get('/', (req, res) => {
+    res.render("employee/addOrEdit", {
+        viewTitle: "ADD STUDENT"
+    });
+});
+router.get('/about', (req, res) => {
+    res.render("employee/about", {
+        viewTitle: ""
+    });
+});
+router.get('/service', (req, res) => {
+    res.render("employee/service", {
+        viewTitle: "service page here"
+    });
+});
+router.post('/', (req, res) => {
+    if (req.body._id == '')
+        insertRecord(req, res);
+        else
+        updateRecord(req, res);
+});
+
+
+function insertRecord(req, res) {
+    var employee = new Employee();
+    employee.fullName = req.body.fullName;
+    employee.FatherName = req.body.FatherName;
+    employee.email = req.body.email;
+    employee.mobile = req.body.mobile;
+    employee.AadhaarCard = req.body.AadhaarCard;
+    employee.BloodGroup = req.body.BloodGroup;
+    employee.calender=req.body.calender;
+    employee.address=req.body.address;
+    employee.city = req.body.city;
+    employee.save((err, doc) => {
+        if (!err)
+            res.redirect('employee/list');
+        else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("employee/addOrEdit", {
+                    viewTitle: "",
+                    employee: req.body
+                });
+            }
+            else
+                console.log('Error during record insertion : ' + err);
+        }
+    });
+}
+
+function updateRecord(req, res) {
+    Employee.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+        if (!err) { res.redirect('employee/list'); }
+        else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("employee/addOrEdit", {
+                    viewTitle: 'UPDATE STUDENT',
+                    employee: req.body
+                });
+            }
+            else
+                console.log('Error during record update : ' + err);
+        }
+    });
+}
+
+
+router.get('/list', (req, res) => {
+    Employee.find((err, docs) => {
+        if (!err) {
+            res.render("employee/list", {
+                list: docs
+            });
+        }
+        else {
+                
+        }
+    });
+});
+
+
+function handleValidationError(err, body) {
+    for (field in err.errors) {
+        switch (err.errors[field].path) {
+            case 'fullName':
+                body['fullNameError'] = err.errors[field].message;
+                break;
+            case 'email':
+                body['emailError'] = err.errors[field].message;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+router.get('/:id', (req, res) => {
+    Employee.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.render("employee/addOrEdit", {
+                viewTitle: "UPDATE STUDENT",
+                employee: doc
+            });
+        }
+    });
+});
+
+router.get('/delete/:id', (req, res) => {
+    Employee.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) {
+            res.redirect('/employee/list');
+        }
+        else { console.log('Error in employee delete :' + err); }
+    });
+});
+
+module.exports = router;
